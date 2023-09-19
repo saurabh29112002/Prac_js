14027809c52e6f8bfc07497c6dcf8de17b817303
Saurabh Mishra <93906383+saurabh29112002@users.noreply.github.com>
2023-09-19 10:36:11 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use  strict"; // treat all JS code as a new version
+
+// alert(3+3) // we are using nodeJS not browser that's why ir does not works
+
+console.log(3+3);
+
+console.log("Saurabh"); // We have to write a code in such way that it should be readable
+
+let name = "saurabh" 
+let age = 21
+let isloggedIn = true
+let state = null
+let city;
+
+// number => 2^54
+// bigint
+// string => ""
+// boolean => t/f
+// null => standalone value
+// undefined => no value assigned
+// symbol => unique
+
+// objects
+
+console.log(typeof "saurabh"); // null ka type objects hai or undefined ka undefined hi hai
