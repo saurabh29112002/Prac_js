2a884682a54b8900bc51d95033ded9c60c2b3e8c
Saurabh Mishra <93906383+saurabh29112002@users.noreply.github.com>
2023-09-24 13:16:47 +0000

    comparison related

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,16 @@
+// console.log(1>0);
+// console.log(2<3);
+// console.log(1==1);
+// console.log(0!=1);
+// console.log(5%2);
+// console.log(5/5);
+
+console.log("3"+3); //isme ye str wale 3 ko number mai convert kar dega
+console.log(3+"3");
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+// same ye null ke sath jo kiya vo undefined ke sath bhi karege but
+// undefined ke case mai kuch bhi operator bhi use karege to bhi kuch nhi hoga
