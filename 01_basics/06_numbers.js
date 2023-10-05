270b6ba15f2c46cf62e89370cb497c5e6cfccbf9
Saurabh Mishra <93906383+saurabh29112002@users.noreply.github.com>
2023-10-05 05:38:29 +0000

    Numbers and math lib

---
+++ 01_basics/06_numbers.js
@@ -0,0 +1,12 @@
+// Basically this program is used to calculate the random number between given min and max
+
+const min = 10
+const max = 20
+
+console.log((Math.floor(Math.random() * (min - max + 10)) + min));
+
+
+const Mynumber = new Number(100)
+console.log(Mynumber);
+
+// need to add more on numbers
